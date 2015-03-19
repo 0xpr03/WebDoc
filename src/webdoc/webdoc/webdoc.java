00906e3267a6f382239b955ca8d4ef9ec7fe04a3
Aron Heinecke <aron.heinecke@t-online.de>
2015-03-19 22:02:23 +0100

    logger test

--- src/webdoc/webdoc/webdoc.java
+++ src/webdoc/webdoc/webdoc.java
@@ -1,8 +1,17 @@
 package webdoc.webdoc;
 
+import webdoc.lib.cLogger;
+
 public class webdoc {
+	
+	private static String sdf = "yyyy-MM-dd HH:mm:ss";
+	private static String log_file_name = "webdoc-client.log";
+	
 	public static void main(String[] args){
-		System.out.println("I'm alive!");
+		cLogger logger = new cLogger(log_file_name, sdf);
+		logger.info("main", "I'm alive!");
+		
+		logger.close();
 	}
 	
 }
