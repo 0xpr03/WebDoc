baf4887ac35665714ff7fd8956eff2c158494f11
Aron Heinecke <aron.heinecke@t-online.de>
2015-03-22 05:49:52 +0100

    logger fix

--- src/webdoc/webdoc/webdoc.java
+++ src/webdoc/webdoc/webdoc.java
@@ -1,5 +1,8 @@
 package webdoc.webdoc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /***
  * Main class
  * @author Aron
@@ -9,6 +12,8 @@ public class webdoc {
 	
 	private static String sdf = "yyyy-MM-dd HH:mm:ss";
 	private static String log_file_name = "webdoc-client.log";
+	
+	private static final Logger logger = LogManager.getLogger(webdoc.class);
 	
 	public static void main(String[] args){
 		System.out.println("Starting..");
