3308b28fd00e8d44b0262d4e4792318dd126e2b1
Jonathan Peper <jonathan.peper@web.de>
2015-03-27 11:37:28 +0100

    Editet Homescreen Signed-off-by: Jonathan Peper <jonathan.peper@web.de>

--- src/webdoc/gui/Homescreen.java
+++ src/webdoc/gui/Homescreen.java
@@ -13,10 +13,19 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextArea;
+import java.awt.ScrollPane;
+import java.awt.TextArea;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import java.awt.Color;
 
 public class Homescreen {
 
 	private JFrame frmWebdoc;
+	private JTextField txtSuche;
 
 	/**
 	 * Launch the application.
@@ -56,16 +65,30 @@ public class Homescreen {
 		JMenu mnMenu = new JMenu("Menu");
 		menuBar.add(mnMenu);
 		
+		JMenuItem mntmNeuerPatient = new JMenuItem("Neuer Patient");
+		mnMenu.add(mntmNeuerPatient);
+		
+		JMenuItem mntmNeuerPartner = new JMenuItem("Neuer Partner");
+		mnMenu.add(mntmNeuerPartner);
+		
 		JMenuItem mntmClose = new JMenuItem("Close");
 		mnMenu.add(mntmClose);
 		
 		JPanel panel = new JPanel();
+		
+		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout groupLayout = new GroupLayout(frmWebdoc.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 707, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(panel, GroupLayout.DEFAULT_SIZE, 707, Short.MAX_VALUE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(128)
+							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 454, Short.MAX_VALUE)
+							.addGap(135)))
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
@@ -73,29 +96,43 @@ public class Homescreen {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(390, Short.MAX_VALUE))
+					.addGap(18)
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 347, Short.MAX_VALUE)
+					.addGap(25))
 		);
 		
+		JTextPane textPane = new JTextPane();
+		textPane.setBackground(new Color(255, 255, 153));
+		scrollPane.setViewportView(textPane);
+		
 		JButton btnNewButton = new JButton("Neuer Partner");
+		btnNewButton.setToolTipText("Erstellen sie einen neuen Partner\r\n");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		
 		JButton btnNeuerPatient = new JButton("Neuer Patient");
+		
+		txtSuche = new JTextField();
+		txtSuche.setText("Suche");
+		txtSuche.setColumns(10);
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addComponent(btnNewButton)
-					.addPreferredGap(ComponentPlacement.RELATED, 495, Short.MAX_VALUE)
+					.addGap(18)
+					.addComponent(txtSuche, GroupLayout.DEFAULT_SIZE, 459, Short.MAX_VALUE)
+					.addGap(18)
 					.addComponent(btnNeuerPatient, GroupLayout.PREFERRED_SIZE, 111, GroupLayout.PREFERRED_SIZE))
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 					.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 35, Short.MAX_VALUE)
-					.addComponent(btnNeuerPatient, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE))
+					.addComponent(btnNeuerPatient, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
+					.addComponent(txtSuche, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 		);
 		panel.setLayout(gl_panel);
 		frmWebdoc.getContentPane().setLayout(groupLayout);
