68349e1bc5e3a5dbd62a3a54d49ab2f9be5104d4
Aron Heinecke <aron.heinecke@t-online.de>
2015-03-31 04:08:00 +0200

    GUI klasse besitzt mehr funktionen & docs

--- src/webdoc/gui/GUI.java
+++ src/webdoc/gui/GUI.java
@@ -1,5 +1,7 @@
 package webdoc.gui;
 
+import java.awt.Component;
+
 import javax.swing.JOptionPane;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,8 +11,44 @@ public class GUI {
 	
 	private static Logger logger = LogManager.getLogger();
 	
+	/**
+	 * Shows a generic error dialog with a yes & no button
+	 * @param parent parent window
+	 * @param message message to display
+	 * @param title title for the dialog
+	 * @return returns 0 for okay, 1 for no / window close
+	 */
+	public static int showErrorYesNoDialog(Component parent, String message, String title){
+		int i = JOptionPane.showConfirmDialog(parent, message, title, JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+		return i;
+	}
+	/**
+	 * Shows a generic error dialog with a yes & no button
+	 * @param message message to display
+	 * @param title title for the dialog
+	 * @return returns 0 for okay, 1 for no / window close
+	 */
 	public static int showErrorYesNoDialog(String message, String title){
 		int i = JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
 		return i;
+	}
+	
+	/**
+	 * Shows an generic error dialog
+	 * @param message message to display
+	 * @param title title for the dialog
+	 */
+	public static void showErrorDialog(String message, String title){
+		JOptionPane.showConfirmDialog(null, message, title, JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
+	}
+	
+	/**
+	 * Shows an generic error dialog
+	 * @param parent parent window
+	 * @param message message to display
+	 * @param title title for the dialog
+	 */
+	public static void showErrorDialog(Component parent, String message, String title){
+		JOptionPane.showConfirmDialog(parent, message, title, JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
 	}
 }
