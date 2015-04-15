9106a766acb14a59069b0ceb3591d087597e133c
Jonathan Peper <jonathan.peper@web.de>
2015-04-15 18:46:01 +0200

    nc Signed-off-by: Jonathan Peper <jonathan.peper@web.de>

--- src/webdoc/gui/WNeuerPatient.java
+++ src/webdoc/gui/WNeuerPatient.java
@@ -18,11 +18,12 @@ public class WNeuerPatient extends JInternalFrame {
 	 */
 	private static final long serialVersionUID = -4647611743598708383L;
 	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField nameDesTieres;
+	private JTextField rasse;
+	private JTextField farbe;
+	private JTextField gewicht;
+	private JTextField identifizierung;
+	public  boolean edit = true;
 
 	/**
 	 * Launch the application.
@@ -134,22 +135,22 @@ public class WNeuerPatient extends JInternalFrame {
 		
 		JLabel lblAllgemeineDaten = new JLabel("Allgemeine Daten");
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
+		nameDesTieres = new JTextField();
+		nameDesTieres.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
+		rasse = new JTextField();
+		rasse.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
+		farbe = new JTextField();
+		farbe.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
+		gewicht = new JTextField();
+		gewicht.setColumns(10);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
+		identifizierung = new JTextField();
+		identifizierung.setColumns(10);
 		
-		JComboBox comboBox = new JComboBox();
+		JComboBox geschlecht = new JComboBox();
 		GroupLayout gl_allgemeineDaten = new GroupLayout(allgemeineDaten);
 		gl_allgemeineDaten.setHorizontalGroup(
 			gl_allgemeineDaten.createParallelGroup(Alignment.TRAILING)
@@ -158,7 +159,7 @@ public class WNeuerPatient extends JInternalFrame {
 						.addGroup(gl_allgemeineDaten.createSequentialGroup()
 							.addComponent(lblIdentifizierung)
 							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(textField_5))
+							.addComponent(identifizierung))
 						.addGroup(gl_allgemeineDaten.createSequentialGroup()
 							.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.LEADING)
 								.addComponent(lblName)
@@ -166,10 +167,10 @@ public class WNeuerPatient extends JInternalFrame {
 								.addComponent(lblGeschlecht))
 							.addGap(3)
 							.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.LEADING)
-								.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)
+								.addComponent(geschlecht, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)
 								.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.LEADING, false)
-									.addComponent(textField_2)
-									.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE))))
+									.addComponent(rasse)
+									.addComponent(nameDesTieres, GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE))))
 						.addGroup(gl_allgemeineDaten.createSequentialGroup()
 							.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.LEADING)
@@ -178,8 +179,8 @@ public class WNeuerPatient extends JInternalFrame {
 								.addComponent(lblGeburtsdatum))
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.LEADING)
-								.addComponent(textField_3)
-								.addComponent(textField_4, GroupLayout.DEFAULT_SIZE, 125, Short.MAX_VALUE))))
+								.addComponent(farbe)
+								.addComponent(gewicht, GroupLayout.DEFAULT_SIZE, 125, Short.MAX_VALUE))))
 					.addContainerGap(40, Short.MAX_VALUE))
 				.addGroup(gl_allgemeineDaten.createSequentialGroup()
 					.addContainerGap(101, Short.MAX_VALUE)
@@ -193,34 +194,44 @@ public class WNeuerPatient extends JInternalFrame {
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblName)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(nameDesTieres, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblRasse)
-						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(rasse, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblGeschlecht)
-						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE))
+						.addComponent(geschlecht, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_allgemeineDaten.createSequentialGroup()
 							.addComponent(lblGeburtsdatum)
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(lblHaarkleidfarbe))
-						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(farbe, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblGewicht)
-						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(gewicht, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_allgemeineDaten.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblIdentifizierung)
-						.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(identifizierung, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap(24, Short.MAX_VALUE))
 		);
 		allgemeineDaten.setLayout(gl_allgemeineDaten);
 		daten.setLayout(gl_daten);
 		getContentPane().setLayout(groupLayout);
+	}
+	private boolean getEdit(){
+		return edit;
+	}
+	public void setEdit(boolean editr){
+		if(editr = true){ 
+			edit = true;
+		}if(editr = false){
+			edit =false;
+		}
 	}
 }
