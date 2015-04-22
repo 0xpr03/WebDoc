dcdba9ed1f7a8f465b4b1cc7cf5d0e24be00276b
Jonathan Peper <jonathan.peper@web.de>
2015-04-22 20:39:17 +0200

    Neue Beziehung präventiv erstellen (noch kein Inhalt)

---
+++ src/webdoc/gui/WNeueBeziehung.java
@@ -0,0 +1,15 @@
+package webdoc.gui;
+
+import javax.swing.JInternalFrame;
+
+
+public class WNeueBeziehung extends JInternalFrame {
+	public WNeueBeziehung() {
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7036704108854786997L;
+
+}
