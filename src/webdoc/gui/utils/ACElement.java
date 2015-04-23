acb0fdffc7d87d8648d3b398757f37999c5e9b85
Aron Heinecke <aron.heinecke@t-online.de>
2015-04-23 23:37:58 +0200

    +element type -getType(long) +getType(elementtype)
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- src/webdoc/gui/utils/ACElement.java
+++ src/webdoc/gui/utils/ACElement.java
@@ -5,18 +5,35 @@ package webdoc.gui.utils;
  * @author "Aron Heinecke"
  */
 public class ACElement {
+	public static enum ElementType{
+		ANIMAL(0),PARTNER(1);
+		private ElementType(int ElementType){
+			this.ElementType = ElementType;
+		}
+		private int ElementType;
+		public int getType(){
+			return this.ElementType;
+		}
+	}
 	
 	private String name;
 	private long id;
+	private ElementType elemType;
 	
-	public ACElement(String name, long id){
+	public ACElement(String name, long id, ElementType elemType){
 		this.name = name;
 		this.id = id;
+		this.elemType = elemType;
 	}
 	
-	public long getType(){
+	public long getID(){
 		return id;
 	}
+	
+	public ElementType getType(){
+		return this.elemType;
+	}
+	
 	@Override
 	public String toString(){
 		return name;
