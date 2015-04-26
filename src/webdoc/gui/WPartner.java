39a529214ca4b00405921ff0e884c2bbe50109b8
Aron Heinecke <aron.heinecke@t-online.de>
2015-04-26 18:13:46 +0200

    *quickfix *rename
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

---
+++ src/webdoc/gui/WPartner.java
@@ -0,0 +1,14 @@
+package webdoc.gui;
+
+public class WPartner extends WNeuerPatient {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2732770291121677822L;
+
+	public WPartner(boolean editable) {
+		super(editable, null, -1);
+	}
+
+}
--- src/webdoc/gui/WPatient.java
+++ src/webdoc/gui/WPatient.java
@@ -11,7 +11,7 @@ public class WPatient extends WNeuerPatient {
 	private static final long serialVersionUID = 2269019202427909267L;	
 
 	public WPatient(boolean editable) {
-		super(editable);
+		super(editable, null, -1);
 	}
 	
 }
