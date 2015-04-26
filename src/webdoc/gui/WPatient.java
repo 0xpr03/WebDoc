e511c3d108c18c16be7089be93d1bdcc069c5dd2
Aron Heinecke <aron.heinecke@t-online.de>
2015-04-26 20:11:54 +0200

    +params
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- src/webdoc/gui/WPatient.java
+++ src/webdoc/gui/WPatient.java
@@ -10,8 +10,8 @@ public class WPatient extends WNeuerPatient {
 	 */
 	private static final long serialVersionUID = 2269019202427909267L;	
 
-	public WPatient(boolean editable) {
-		super(editable, null, -1);
+	public WPatient(boolean editable, WHomescreen whs, long id) {
+		super(editable, whs, id);
 	}
 	
 }
