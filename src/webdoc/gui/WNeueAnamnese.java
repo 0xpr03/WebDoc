25135adafe59d6acf63b303e2a02c37e59073279
Jonathan Peper <jonathan.peper@web.de>
2015-04-28 08:12:27 +0200

    temp

--- src/webdoc/gui/WNeueAnamnese.java
+++ src/webdoc/gui/WNeueAnamnese.java
@@ -21,6 +21,8 @@ import javax.swing.JSpinner;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.SpinnerDateModel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
 
 public class WNeueAnamnese extends JInternalFrame {
 
@@ -30,6 +32,7 @@ public class WNeueAnamnese extends JInternalFrame {
 	private JTextField textField_6;
 	private JTextField textField_7;
 	private JTextField textField_8;
+	private JSpinner spinBirthdate_1;
 
 	/**
 	 * Launch the application.
@@ -61,30 +64,44 @@ public class WNeueAnamnese extends JInternalFrame {
 		
 		JPanel pHeader = new JPanel();
 		
-		JPanel pContent = new JPanel();
+		JPanel tabber = new JPanel();
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setLayout(new MigLayout("", "[][]", "[]"));
+		
+		JButton btnOk = new JButton("Ok");
+		panel_3.add(btnOk, "cell 0 0");
+		
+		JButton btnCancel = new JButton("Cancel");
+		panel_3.add(btnCancel, "cell 1 0");
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addComponent(pHeader, GroupLayout.DEFAULT_SIZE, 861, Short.MAX_VALUE)
+					.addComponent(pHeader, GroupLayout.DEFAULT_SIZE, 1339, Short.MAX_VALUE)
 					.addGap(2))
 				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(18)
+					.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 247, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(1076, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(pContent, GroupLayout.DEFAULT_SIZE, 853, Short.MAX_VALUE)
-					.addContainerGap())
+					.addComponent(tabber, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addComponent(pHeader, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(pContent, GroupLayout.DEFAULT_SIZE, 425, Short.MAX_VALUE)
-					.addGap(59))
+					.addComponent(tabber, GroupLayout.DEFAULT_SIZE, 585, Short.MAX_VALUE)
+					.addGap(1)
+					.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 		);
-		pContent.setLayout(new BoxLayout(pContent, BoxLayout.X_AXIS));
+		
+		JPanel pContent = new JPanel();
 		
 		JPanel panel_2 = new JPanel();
-		pContent.add(panel_2);
 		
 		JLabel label = new JLabel("Änderungen im Familiengefüge:");
 		
@@ -110,22 +127,17 @@ public class WNeueAnamnese extends JInternalFrame {
 		
 		textField_7 = new JTextField();
 		textField_7.setColumns(10);
-		
-		JSpinner spinBirthdate = new JSpinner();
-		SpinnerDateModel model = new SpinnerDateModel();
-		model.setCalendarField(Calendar.MINUTE);
-		spinBirthdate = new JSpinner();
-		spinBirthdate.setModel(model);
-		DateEditor dateEditor = new JSpinner.DateEditor(spinBirthdate, "dd-MM-yyyy");
-		spinBirthdate.setEditor(dateEditor);
-		spinBirthdate.setEnabled(editable);
+		spinBirthdate_1 = new JSpinner();
+		spinBirthdate_1.setModel(model);
+		DateEditor dateEditor = new JSpinner.DateEditor(spinBirthdate_1, "dd-MM-yyyy");
+		spinBirthdate_1.setEditor(dateEditor);
+		spinBirthdate_1.setEnabled(editable);
 		
 		textField_8 = new JTextField();
 		textField_8.setColumns(10);
 		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
 		gl_panel_2.setHorizontalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 421, Short.MAX_VALUE)
 				.addGroup(gl_panel_2.createSequentialGroup()
 					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
 						.addComponent(label_1)
@@ -144,7 +156,7 @@ public class WNeueAnamnese extends JInternalFrame {
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING, false)
 						.addComponent(textField_7, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
-						.addComponent(spinBirthdate)
+						.addComponent(spinBirthdate_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(textField_8))
 					.addGap(167))
 				.addGroup(gl_panel_2.createSequentialGroup()
@@ -153,11 +165,10 @@ public class WNeueAnamnese extends JInternalFrame {
 				.addGroup(gl_panel_2.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(scrollPane_5, GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE)
-					.addGap(15))
+					.addContainerGap())
 		);
 		gl_panel_2.setVerticalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 467, Short.MAX_VALUE)
 				.addGroup(gl_panel_2.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
@@ -176,7 +187,7 @@ public class WNeueAnamnese extends JInternalFrame {
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
 						.addComponent(label_4)
-						.addComponent(spinBirthdate, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(spinBirthdate_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
 						.addComponent(label_5)
@@ -184,8 +195,8 @@ public class WNeueAnamnese extends JInternalFrame {
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(label)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollPane_5, GroupLayout.PREFERRED_SIZE, 236, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(13, Short.MAX_VALUE))
+					.addComponent(scrollPane_5, GroupLayout.DEFAULT_SIZE, 280, Short.MAX_VALUE)
+					.addGap(1))
 		);
 		
 		JTextPane textPane = new JTextPane();
@@ -193,7 +204,6 @@ public class WNeueAnamnese extends JInternalFrame {
 		panel_2.setLayout(gl_panel_2);
 		
 		JPanel panel_1 = new JPanel();
-		pContent.add(panel_1);
 		
 		JLabel lblAuslandsaufenthalte = new JLabel("Auslandsaufenthalte:");
 		
@@ -215,18 +225,18 @@ public class WNeueAnamnese extends JInternalFrame {
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_panel_1.createSequentialGroup()
 							.addComponent(lblVerletzungen)
 							.addGap(56)
 							.addComponent(scrollPane_3, GroupLayout.DEFAULT_SIZE, 273, Short.MAX_VALUE)
 							.addContainerGap())
-						.addGroup(gl_panel_1.createSequentialGroup()
+						.addGroup(Alignment.LEADING, gl_panel_1.createSequentialGroup()
 							.addComponent(lblNarben)
 							.addGap(84)
-							.addComponent(scrollPane_4, GroupLayout.DEFAULT_SIZE, 183, Short.MAX_VALUE)
-							.addGap(100))
-						.addGroup(Alignment.TRAILING, gl_panel_1.createSequentialGroup()
+							.addComponent(scrollPane_4, GroupLayout.DEFAULT_SIZE, 273, Short.MAX_VALUE)
+							.addContainerGap())
+						.addGroup(gl_panel_1.createSequentialGroup()
 							.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
 								.addGroup(gl_panel_1.createSequentialGroup()
 									.addComponent(lblVerhaltensauflligkeiten)
@@ -256,14 +266,13 @@ public class WNeueAnamnese extends JInternalFrame {
 						.addGroup(gl_panel_1.createSequentialGroup()
 							.addGap(6)
 							.addComponent(scrollPane_3, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)))
-					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panel_1.createSequentialGroup()
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(scrollPane_4, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE))
+							.addGap(33)
+							.addComponent(lblNarben))
 						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGap(27)
-							.addComponent(lblNarben)))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(scrollPane_4, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap())
 		);
 		
@@ -281,7 +290,6 @@ public class WNeueAnamnese extends JInternalFrame {
 		panel_1.setLayout(gl_panel_1);
 		
 		JPanel panel = new JPanel();
-		pContent.add(panel);
 		
 		JLabel lblInfektionserkrankungen = new JLabel("Infektionserkrankungen:");
 		
@@ -290,6 +298,10 @@ public class WNeueAnamnese extends JInternalFrame {
 		JLabel lblRegelmigeImpfungen = new JLabel("Regelmäßige Impfungen:");
 		
 		JScrollPane scrollPane_6 = new JScrollPane();
+		
+		JLabel lblHerzkreislauf = new JLabel("Herz/Kreislauf:");
+		
+		JScrollPane scrollPane_7 = new JScrollPane();
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
@@ -297,12 +309,18 @@ public class WNeueAnamnese extends JInternalFrame {
 					.addContainerGap()
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 						.addComponent(lblInfektionserkrankungen)
-						.addComponent(lblRegelmigeImpfungen, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE))
+						.addComponent(lblRegelmigeImpfungen, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblHerzkreislauf))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addComponent(scrollPane_6)
-						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE))
-					.addGap(48))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addComponent(scrollPane_6, GroupLayout.DEFAULT_SIZE, 299, Short.MAX_VALUE)
+								.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE))
+							.addGap(48))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(scrollPane_7, GroupLayout.PREFERRED_SIZE, 299, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap(48, Short.MAX_VALUE))))
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
@@ -313,10 +331,19 @@ public class WNeueAnamnese extends JInternalFrame {
 						.addComponent(lblInfektionserkrankungen))
 					.addGap(13)
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblRegelmigeImpfungen)
-						.addComponent(scrollPane_6, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(235, Short.MAX_VALUE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(lblRegelmigeImpfungen)
+							.addGap(102)
+							.addComponent(lblHerzkreislauf))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(scrollPane_6, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(scrollPane_7, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(120, Short.MAX_VALUE))
 		);
+		
+		JTextPane textPane_7 = new JTextPane();
+		scrollPane_7.setViewportView(textPane_7);
 		
 		JTextPane textPane_6 = new JTextPane();
 		scrollPane_6.setViewportView(textPane_6);
@@ -324,6 +351,53 @@ public class WNeueAnamnese extends JInternalFrame {
 		JTextPane textPane_5 = new JTextPane();
 		scrollPane.setViewportView(textPane_5);
 		panel.setLayout(gl_panel);
+		GroupLayout gl_pContent = new GroupLayout(pContent);
+		gl_pContent.setHorizontalGroup(
+			gl_pContent.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pContent.createSequentialGroup()
+					.addGap(1)
+					.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 421, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 406, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 487, Short.MAX_VALUE)
+					.addGap(1))
+		);
+		gl_pContent.setVerticalGroup(
+			gl_pContent.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pContent.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_pContent.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, gl_pContent.createSequentialGroup()
+							.addComponent(panel, GroupLayout.DEFAULT_SIZE, 556, Short.MAX_VALUE)
+							.addGap(21))
+						.addGroup(Alignment.TRAILING, gl_pContent.createSequentialGroup()
+							.addGroup(gl_pContent.createParallelGroup(Alignment.TRAILING)
+								.addComponent(panel_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 556, Short.MAX_VALUE)
+								.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 556, Short.MAX_VALUE))
+							.addGap(21))))
+		);
+		pContent.setLayout(gl_pContent);
+		GroupLayout gl_tabber = new GroupLayout(tabber);
+		gl_tabber.setHorizontalGroup(
+			gl_tabber.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_tabber.createSequentialGroup()
+					.addGap(1)
+					.addComponent(pContent, GroupLayout.DEFAULT_SIZE, 1307, Short.MAX_VALUE)
+					.addGap(1))
+		);
+		gl_tabber.setVerticalGroup(
+			gl_tabber.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_tabber.createSequentialGroup()
+					.addGap(1)
+					.addComponent(pContent, GroupLayout.DEFAULT_SIZE, 574, Short.MAX_VALUE)
+					.addGap(1))
+		);
+		tabber.setLayout(gl_tabber);
+		
+		JSpinner spinBirthdate = new JSpinner();
+		SpinnerDateModel model = new SpinnerDateModel();
+		model.setCalendarField(Calendar.MINUTE);
 		getContentPane().setLayout(groupLayout);
 	}
 
@@ -333,7 +407,7 @@ public class WNeueAnamnese extends JInternalFrame {
 	private void initialize(boolean editable, String strName) {
 		frame = new JFrame();
 		setTitle("Anamnese von " + strName);
-		setBounds(100, 100, 1351, 571);
+		setBounds(100, 100, 1351, 687);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
