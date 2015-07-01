b71c41736e75be243e89664499ebe1aa5e4a9a97
Aron Heinecke <aron.heinecke@t-online.de>
2015-07-01 16:38:10 +0200

    *table to list, first part +title

--- src/webdoc/gui/WVerwaltung.java
+++ src/webdoc/gui/WVerwaltung.java
@@ -16,6 +16,7 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.table.TableRowSorter;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,6 +25,8 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -31,17 +34,22 @@ import net.miginfocom.swing.MigLayout;
 import webdoc.gui.utils.EnumObject;
 import webdoc.gui.utils.EnumObject.EnumType;
 import webdoc.gui.utils.JSearchTextField;
+import webdoc.gui.utils.PatientTableModel;
 import webdoc.gui.utils.TDListElement;
 import webdoc.gui.utils.WModelPane;
 import webdoc.lib.Database;
 import webdoc.lib.GUIManager;
-
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
 
 public class WVerwaltung extends WModelPane {
 
 	private static final long serialVersionUID = 4589284070560679651L;
 	private boolean editable;
-	private JList<TDListElement> list;
+	private JTable table;
 	private JButton btnSchliesen;
 	private JButton btnNeueBehandlungsart;
 	private JPanel panel_2;
@@ -51,6 +59,8 @@ public class WVerwaltung extends WModelPane {
 	
 	public WVerwaltung() {
 		super(serialVersionUID);
+		setTitle("DB Admin Panel");
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		
 		setBounds(0, 0, 329, 543);
 		setClosable(true);
@@ -69,20 +79,30 @@ public class WVerwaltung extends WModelPane {
 		JPanel panel_1 = new JPanel();
 		getContentPane().add(panel_1, BorderLayout.CENTER);
 		
-		list = new JList<TDListElement>(new DefaultListModel<TDListElement>());
+		//table = new JTable(model);
+		table.setShowGrid(false);
+		table.setShowHorizontalLines(false);
+		table.setShowVerticalLines(false);
+		table.setRowMargin(0);
+		table.setIntercellSpacing(new Dimension(0, 0));
+		table.setFillsViewportHeight(true);
+//		TableRowSorter<PatientTableModel> sorter = new TableRowSorter<PatientTableModel>(
+//				model);
+		//table.setRowSorter(sorter);
+		JScrollPane scrollPaneTable = new JScrollPane(table);
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
 		gl_panel_1.setHorizontalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
 					.addGap(1)
-					.addComponent(list, GroupLayout.DEFAULT_SIZE, 242, Short.MAX_VALUE)
+					.addComponent(scrollPaneTable, GroupLayout.DEFAULT_SIZE, 242, Short.MAX_VALUE)
 					.addGap(1))
 		);
 		gl_panel_1.setVerticalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(Alignment.TRAILING, gl_panel_1.createSequentialGroup()
 					.addGap(1)
-					.addComponent(list, GroupLayout.DEFAULT_SIZE, 377, Short.MAX_VALUE)
+					.addComponent(scrollPaneTable, GroupLayout.DEFAULT_SIZE, 377, Short.MAX_VALUE)
 					.addGap(1))
 		);
 		panel_1.setLayout(gl_panel_1);
@@ -93,23 +113,32 @@ public class WVerwaltung extends WModelPane {
 		searchTextField = new JSearchTextField(false);
 		
 		cBAuswahl = new JComboBox<EnumObject>();
+		class ItemChangeListener implements ItemListener {
+			@Override
+			public void itemStateChanged(ItemEvent event) {
+				if (event.getStateChange() == ItemEvent.SELECTED) {
+					loadData();
+				}
+			}
+		}
+		cBAuswahl.addItemListener(new ItemChangeListener());
 		cBAuswahl.setModel((new DefaultComboBoxModel<EnumObject>(new EnumObject[] {new EnumObject("Behandlungsarten", EnumType.A), new EnumObject("Patienten", EnumType.B), new EnumObject("Partner", EnumType.C)})));
 		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
 		gl_panel_2.setHorizontalGroup(
 			gl_panel_2.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel_2.createSequentialGroup()
 					.addGap(1)
-					.addComponent(cBAuswahl, 0, 115, Short.MAX_VALUE)
+					.addComponent(cBAuswahl, 0, 113, Short.MAX_VALUE)
 					.addGap(1)
-					.addComponent(searchTextField, GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE))
+					.addComponent(searchTextField, GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE))
 		);
 		gl_panel_2.setVerticalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_2.createSequentialGroup()
 					.addGap(1)
-					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
-						.addComponent(searchTextField, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(cBAuswahl))
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE, false)
+						.addComponent(searchTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(cBAuswahl, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(1))
 		);
 		panel_2.setLayout(gl_panel_2);
@@ -143,7 +172,7 @@ public class WVerwaltung extends WModelPane {
 							ResultSet rs = Database.getTableEntry(element.getType());
 							if(rs != null){
 								try{
-									DefaultListModel<TDListElement> model = (DefaultListModel<TDListElement>) list.getModel();
+									DefaultListModel<TDListElement> model = (DefaultListModel<TDListElement>) table.getModel();
 									model.clear();
 									while(rs.next()){
 										model.addElement(new TDListElement(rs.getLong(1), rs.getString(2)));
